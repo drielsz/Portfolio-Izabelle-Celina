76f9a0ee3b02f266fcdcc5b13e170a8c186d698f
drielsz <adriellaurentino4@gmail.com>
2023-05-07 23:28:35 -0300

    interface att

--- js/scroll.js
+++ js/scroll.js
@@ -15,3 +15,15 @@ Array.from(path).forEach((items) => {
     })
 })
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hiddensection');
+hiddenElements.forEach((el) => observer.observe(el));
